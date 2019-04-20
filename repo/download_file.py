129775958def9b234747812bfd82f22d74887b8e
Ubuntu <ubuntu@ip-172-31-86-159.ec2.internal>
2019-04-20 23:38:32 +0000

    added logic to get file count

--- repo/download_file.py
+++ repo/download_file.py
@@ -1,6 +1,25 @@
 import boto3
+import botocore
+import os
 
 def run(bucket, file):
-    destination = '/home/ubuntu/DownloadFolder/'+file
-    s3 = boto.resource('s3')
-    s3.meta.client.download_file(bucket, file, destination)
+    print("the file name is : ")
+    print(file)
+    print(bucket)
+    destination = '/DownloadFolder/'+file
+    #print(destination)
+    s3 = boto3.resource('s3')
+    #s3.meta.client.download_file(bucket, file, destination)
+    op_dir = os.path.join('/home/ubuntu/repo/DownloadFolder', file)
+    print(op_dir)
+    s3.meta.client.download_file(bucket, file, op_dir)
+
+
+def list_files(bucket, file):
+    keys = []
+    kwargs = {'Bucket': bucket, 'Prefix': file}
+    s3 = boto3.client('s3')
+    resp = s3.list_objects(**kwargs)
+    for file in resp['Contents']:
+        keys.append(file['Key'])
+    return len(keys)
